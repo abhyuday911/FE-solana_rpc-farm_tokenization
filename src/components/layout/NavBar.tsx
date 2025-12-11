4f7212523bed06976a29d7a747e473f6aa314f59
Abhyuday <abhyuday911@gmail.com>
2025-12-11 16:39:22 +0530

    feat: added constants [idl , PROGRAM_ID]

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -3,7 +3,7 @@ import { WalletMultiButton } from "../providers/solana-provider";
 
 const NavBar = () => {
   return (
-    <div className='flex justify-between px-20 py-8 backdrop-blur-2xl'>
+    <div className="flex justify-between px-20 py-8 backdrop-blur-2xl">
       <div className="w-20"></div>
       <WalletMultiButton />
     </div>
---
+++ src/constants/ProgramId.ts
@@ -0,0 +1,2 @@
+import { PublicKey } from '@solana/web3.js';
+export const PROGRAM_ID = new PublicKey("7SBE29SotX496Tdhr69uTQrG6ADP9Ds3QyAVozFAUno1")
