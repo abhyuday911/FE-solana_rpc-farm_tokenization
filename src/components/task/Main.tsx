650f3b07459696876aa4fb13a1cc44a05e09e4c6
Abhyuday <abhyuday911@gmail.com>
2025-12-15 15:12:07 +0530

    inital template for rpc practice

--- src/components/task/Main.tsx
+++ src/components/task/Main.tsx
@@ -1,46 +1,10 @@
 "use client";
 import { Button } from "../ui/button";
-import { useInitializeFarm } from "@/hooks/useFarm";
-import { useState } from "react";
-import { PublicKey } from "@solana/web3.js";
-import Link from "next/link";
-import { toast } from "sonner";
 
 const Main = () => {
-  const { initializeFarm, loading, isReady } = useInitializeFarm();
-  const [farmPubKey, setFarmPubKey] = useState<PublicKey | null>(null);
-
-  const createFarm = async () => {
-    try {
-      const { farm } = await initializeFarm();
-      setFarmPubKey(farm);
-      toast.success("Farm created successfully");
-    } catch (err) {
-      toast.error("Failed to create farm");
-      console.error(err);
-    }
-  };
-
   return (
     <div className="space-y-4 flex flex-col items-center justify-center">
-      <>
-        {farmPubKey && (
-          <>
-            <h1 className="text-xl px-2">{farmPubKey?.toString()}</h1>
-            <Button asChild variant={"secondary"}>
-              <Link
-                href={`https://explorer.solana.com/address/${farmPubKey?.toString()}?cluster=devnet`}
-                target="_blank"
-              >
-                Solana Explorer Link
-              </Link>
-            </Button>
-          </>
-        )}
-      </>
-      <Button onClick={createFarm} disabled= {!isReady || loading}>
-        {loading ? "Creating Farm..." : "Create Farm"}
-      </Button>
+      <Button>Create Farm</Button>
     </div>
   );
 };
--- src/constants/ProgramId.ts
+++
@@ -1,3 +0,0 @@
-import { PublicKey } from '@solana/web3.js';
-import { FARM_IDL } from './idl';
-export const PROGRAM_ID = new PublicKey(FARM_IDL.address)
